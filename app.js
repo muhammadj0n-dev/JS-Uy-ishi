2f0aed9a8feb5479cfb91cc8575d3e7896baa174
muhammadj0n-dev <153054724+muhammadj0n-dev@users.noreply.github.com>
2024-01-18 22:20:45 +0500

    MASALALAR

---
+++ app.js
@@ -0,0 +1,101 @@
+// function yigindiCAlc(n){
+//     let yigindi = 0;
+//     for( let i = 1; i <= n; i++){
+//         yigindi += 1/i
+        
+        
+//     }
+//     return yigindi
+// }
+// console.log(yigindiCAlc(10))
+
+
+// <--------- Uy ishi ---------->
+// 1
+// function abs(a){
+//     return a**3
+// }
+// console.log(abs(10));
+// 2
+// function yigindi(a,b){
+//     return a+b
+// }
+// console.log(yigindi(5,6))
+// 3
+// function ayirma(a,b){
+//     return a-b;
+// }
+// console.log(ayirma(100,50))
+// 4
+// function daraja(a){
+//     console.log(a**2);
+//     console.log(a**3);
+//     console.log(a**4);
+// }
+
+// daraja(5);
+// 5
+// function son(n){
+//  for( let i = 1; i <= n; i++){
+//     if (i % 2 == 1){
+//         console.log( i )
+//     }
+//  }
+// }
+// console.log(son(10));
+// 6
+// function son(n){
+//     for( let i = 1; i <= n; i++){
+//        if (i % 2 == 0){
+//            console.log( i )
+//        }
+//     }
+//    }
+//    console.log(son(10));
+// 7
+// function num(n){
+//     console.log(n++);
+//     console.log(n--);
+// }
+// console.log(num(50));
+// 8
+// function son(n){
+//     for( let i = 1; i <= n; i++){
+//        if (n % i == 0){
+//            console.log( i )
+//        }
+//     }
+//    }
+//    console.log(son(10));
+// 9
+// function mirjon(a,b,i){
+//     if (i == "+"){
+//         console.log(a+b);
+//     }else if(i == "-"){
+//         console.log(a-b);
+//     }else if(i == "*"){
+//         console.log(a*b);
+//     }else if(i == "/"){
+//         console.log(a/b);
+//     }else {
+//         console.log("invalid " )
+//     }
+    
+// }
+// mirjon(8,4,"-")2
+//10
+// function printTable() {
+//     for (let i = 1; i < 9; i++) {
+//         console.log(`\n${i}`);
+//         for (let j = 1; j <= 9; j++) {
+//             console.log(`${i} x ${j} = ${i * j}`);
+//         }
+//     }
+// }
+
+// // Funksiya chaqirilishi
+// printTable();
+
+// <------ Uy ishi o'z yakuniga yetti --------->
+
+// 
